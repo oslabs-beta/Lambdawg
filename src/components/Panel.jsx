577582eb114fa3f1ceb8daf1207a1c1fbe8f23fa
Chanda Gonet <chandagonet@gmail.com>
2023-02-28 16:33:30 -0500

    Render function names from api to panel

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,21 +1,25 @@
 
 import React, { useState, useEffect } from 'react';
+import Microservice from '../components/Microservice.jsx';
 
 const Panel = (props) => {
 
-  const { setPanelFullScreen, panelFullScreen } = props;
-
+  const { setPanelFullScreen, panelFullScreen, msNames } = props;
   // get the list of functions in heirarchal order and display them as a list of 'buttons' >
   // onClick a button will expand display the functions metrics in the same window, onClick it will hide them
   // the corresponding node on the diagram will be highlighted
-
+  const names = []
+  msNames.forEach((name) => {
+    names.push(<Microservice name={name}/>)
+  })
 
   
     return(
       <div id="panel-wrapper" className={panelFullScreen? 'full-screen' : 'collapse-screen'}>
+        {names}
         <div id='testClick'></div>
-        <ul id='panelUl'>
-          <li> This is a thing </li>
+        {/* <ul id='panelUl'> */}
+          {/* <li> This is a thing </li>
             <ul>
             <li> This is a thing </li>
             <li> This is a thing </li>
@@ -36,8 +40,8 @@ const Panel = (props) => {
             </ul>
           <li> This is a thing </li>
           <li> This is a thing </li>
-          <li> This is a thing </li>
-        </ul>
+          <li> This is a thing </li> */}
+        {/* </ul> */}
 
 
       </div>
