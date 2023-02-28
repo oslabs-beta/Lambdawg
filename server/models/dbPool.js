545d3630da20bc51d645f5c883a2281845669f16
Ted Gusek <theohgusek@gmail.com>
2023-02-28 11:44:29 -0500

    env file, updated gitignore

--- server/models/dbPool.js
+++ server/models/dbPool.js
@@ -1,17 +1,13 @@
 const { Pool } = require('pg');
+const dotenv = require('dotenv').config();
 
-const PG_URI =
-  //   'postgres://fzlocmxc:cSVvrq52FYy9AXvuGc2r6lwqoFnzEH82@trumpet.db.elephantsql.com/fzlocmxc';
-  'postgres://fzlocmxc:cSVvrq52FYy9AXvuGc2r6lwqoFnzEH82@trumpet.db.elephantsql.com/fzlocmxc';
+const { PG_URI } = process.env;
 
 //create a new pool using the connection string above that brings us to our database
 
 const pool = new Pool({
   connectionString: PG_URI,
 });
-
-//Wouldn't you love to know what our databse looks like?
-//Well, ass soon as i start to poulate it, I will leave details here as to what the structure looks like
 
 //Now we need to export an object that has a query method in it, that we'll name query
 //this method will return the invocation of the pool.query() after logging the query
