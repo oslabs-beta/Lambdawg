7c940d930115bbd1679f7c4cb591ec9c2d404bd6
Chanda Gonet <chandagonet@gmail.com>
2023-02-28 17:55:17 -0500

    Fix mobile map, panel scroll

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -10,40 +10,13 @@ const Panel = (props) => {
   // the corresponding node on the diagram will be highlighted
   const names = []
   msNames.forEach((name) => {
-    names.push(<Microservice name={name}/>)
+    names.push(<Microservice name={name} className='panel-names'/>)
   })
 
   
     return(
       <div id="panel-wrapper" className={panelFullScreen? 'full-screen' : 'collapse-screen'}>
         {names}
-        {/* <div id='testClick'></div> */}
-        {/* <ul id='panelUl'> */}
-          {/* <li> This is a thing </li>
-            <ul>
-            <li> This is a thing </li>
-            <li> This is a thing </li>
-            <li> This is a thing </li>
-            </ul>
-          <li> This is a thing </li>
-          <li> This is a thing </li>
-          <li> This is a thing </li>
-          <li> This is a thing </li>
-          <ul>
-            <li> This is a thing </li>
-            </ul>
-          <li> This is a thing </li>
-          <li> This is a thing </li>
-          <li> This is a thing </li>
-          <ul>
-            <li> This is a thing </li>
-            </ul>
-          <li> This is a thing </li>
-          <li> This is a thing </li>
-          <li> This is a thing </li> */}
-        {/* </ul> */}
-
-
       </div>
     )
   }
