26f263756baa66217222c5e0ce424619d7f2d838
Chanda Gonet <chandagonet@gmail.com>
2023-03-06 20:01:16 -0500

    Authenticates via cookie

--- server/controllers/cookieControllers.js
+++ server/controllers/cookieControllers.js
@@ -10,15 +10,21 @@ cookieControllers.setCookie = (req, res, next) => {
 
   //sends the token as a cookie
   // , secure: true  // If I want to make it via https only add this in the object below
-  res.cookie('jwt', accessToken, { httpOnly: true });
-
+  // res.cookie('jwt', accessToken);
+  res.cookie('jwt', accessToken, {
+    httpOnly: false,
+    sameSite: 'none',
+    secure: true
+  });
   return next();
 };
 
 cookieControllers.authenticateCookie = (req, res, next) => {
+  console.log('inside cookie controller', JSON.stringify(req.cookie))
+
   //assigns the jwt string to authHeader
   const authHeader = req.headers.cookie;
-
+  console.log('authheader', authHeader)
   //this is checking to see if we have a cookie, and if we do, assign the sliced
   //cookie to token (We're slicing it to remove the key name, in our case 'jwt=')
   const token = authHeader && authHeader.slice(4);
@@ -27,17 +33,16 @@ cookieControllers.authenticateCookie = (req, res, next) => {
   //if it does, verifies if its legit
   //if not legit sends back an error
   if (!token) {
-    return res
-      .status(401)
-      .json({ message: 'Get outta Here! You are not AUTHORIZED' });
+    return res.status(401);
+    // .json({ message: 'Get outta Here! You are not AUTHORIZED' });
   }
   try {
     const decoded = jwt.verify(token, process.env.ACCESS_SECRET_TOKEN);
+    res.locals.user_name = decoded;
     return next();
   } catch (err) {
-    return res
-      .status(401)
-      .json({ message: 'Get outta Here! You are not AUTHORIZED' });
+    return res.status(401);
+    // .json({ message: 'Get outta Here! You are not AUTHORIZED' });
   }
 };
 
