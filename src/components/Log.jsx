a470a8f3c636b9b4106bb4170633fb1d2dfa9799
Chanda Gonet <chandagonet@gmail.com>
2023-03-10 18:14:56 -0500

    Add styling to logs in dataWindow

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -7,11 +7,13 @@ function Log(props) {
   console.log('inside LOG', logs)
   return (
     <div id={functionName}>
-      <h2>{functionName}</h2>
-      <ul>
+      <h2 className='log-name'>{functionName}</h2>
+      <ul id='log-ul'>
         {logs.map((log, index) => (
-          <li key={index}>
-            <div>{log.timestamp}: {log.message}</div>
+          <li key={index} className='log-wrap'>
+            <div className='msg-name'>
+              <span className='time-stamp'>{log.timestamp}</span>:<br />
+              {log.message}</div>
           </li>
         ))}
       </ul>
