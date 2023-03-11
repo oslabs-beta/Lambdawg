b65ee874272d8d1d8293a5e763ff4dd665a56378
EPSparky <ericayeong94@gmail.com>
2023-03-10 20:50:57 -0500

    fixed STS error

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,91 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DashboardContainer from "./containers/DashboardContainer.jsx";
+import LandingPageContainer from "./containers/LandingPageContainer.jsx";
+import Auth from "./components/Auth.jsx";
+import Docs from "./components/Docs.jsx";
+import SettingsContainer from "./containers/SettingsContainer.jsx";
+import Navbar from "./components/Navbar.jsx";
 
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import DashboardContainer from './containers/DashboardContainer.jsx';
-import LandingPageContainer from './containers/LandingPageContainer.jsx';
-import Auth from './components/Auth.jsx';
-import Docs from './components/Docs.jsx';
-import SettingsContainer from './containers/SettingsContainer.jsx';
-import Navbar from './components/Navbar.jsx';
-
-import './styles/application.scss';
+import "./styles/application.scss";
 
 const App = (props) => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [user, setUser] = useState({});
 
-  console.log('app.jsx, user in state', user)
+  console.log("app.jsx, user in state", user);
   // check for cookie and if found, log user in and save userdata
   useEffect(() => {
     const checkAuth = async () => {
-     
-        try {
-          const response = await fetch('http://localhost:3000/api/', {
-            credentials: 'include'
-          })
-          if (response.ok) {
-            const data = await response.json();
-            console.log('jwt fetch ok', data[0]);
-            const { user_name, full_name, email, _id, arn, region } = data[0]
-            setUser({
-              full_name: full_name,
-              user_name: user_name, 
-              email: email,
-              _id: _id,
-              arn: arn,
-              region: region
-            })
-            setLoggedIn(true);
-          } 
-          else {
-            console.error('JWT app.jsx Unable to authenticate jwt');
-            setLoggedIn(false);
-          }
-        } 
-        catch (error) {
-          console.error('JWT app.jsx', error);
+      try {
+        const response = await fetch("http://localhost:3000/api/", {
+          credentials: "include",
+        });
+        if (response.ok) {
+          const data = await response.json();
+          console.log("jwt fetch ok", data[0]);
+          const { user_name, full_name, email, _id, arn, region } = data[0];
+          setUser({
+            full_name: full_name,
+            user_name: user_name,
+            email: email,
+            _id: _id,
+            arn: arn,
+            region: region,
+          });
+          setLoggedIn(true);
+        } else {
+          console.error("JWT app.jsx Unable to authenticate jwt");
           setLoggedIn(false);
         }
+      } catch (error) {
+        console.error("JWT app.jsx", error);
+        setLoggedIn(false);
+      }
     };
     checkAuth();
   }, []);
 
-
-
   return (
     <div className="router">
-      <Navbar loggedIn={loggedIn} setLoggedIn={setLoggedIn} user={user}/>
+      <Navbar loggedIn={loggedIn} setLoggedIn={setLoggedIn} user={user} />
       <div className="routerMain" id="content">
         <Routes>
-          
-          <Route exact path="/dashboard" element={
-            loggedIn ? <DashboardContainer loggedIn={loggedIn} user={user}/> : 
-            <Auth loggedIn={loggedIn} setLoggedIn={setLoggedIn} user={user} setUser={setUser}/>
-          } /> 
-          <Route exact path="/settings" element={
-            loggedIn ? <SettingsContainer loggedIn={loggedIn} user={user} setUser={setUser}/> : 
-            <Auth loggedIn={loggedIn} setLoggedIn={setLoggedIn} user={user} setUser={setUser}/>
-          } /> 
-          <Route exact path="/auth" element={
-            loggedIn ? <DashboardContainer loggedIn={loggedIn} user={user}/> : 
-            <Auth loggedIn={loggedIn} setLoggedIn={setLoggedIn} user={user} setUser={setUser}/>
-          } /> 
-          <Route exact path="/docs" element={<Docs/>} />
-          <Route exact path="/" element={<LandingPageContainer/>} /> 
-
+          <Route
+            exact
+            path="/dashboard"
+            element={
+              loggedIn ? (
+                <DashboardContainer loggedIn={loggedIn} user={user} />
+              ) : (
+                <Auth loggedIn={loggedIn} setLoggedIn={setLoggedIn} user={user} setUser={setUser} />
+              )
+            }
+          />
+          <Route
+            exact
+            path="/settings"
+            element={
+              loggedIn ? (
+                <SettingsContainer loggedIn={loggedIn} user={user} setUser={setUser} />
+              ) : (
+                <Auth loggedIn={loggedIn} setLoggedIn={setLoggedIn} user={user} setUser={setUser} />
+              )
+            }
+          />
+          <Route
+            exact
+            path="/auth"
+            element={
+              loggedIn ? (
+                <DashboardContainer loggedIn={loggedIn} user={user} />
+              ) : (
+                <Auth loggedIn={loggedIn} setLoggedIn={setLoggedIn} user={user} setUser={setUser} />
+              )
+            }
+          />
+          <Route exact path="/docs" element={<Docs />} />
+          <Route exact path="/" element={<LandingPageContainer />} />
         </Routes>
       </div>
     </div>
