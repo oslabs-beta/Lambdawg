d28c218e7ff6247836ebd3aa5605860516082fc5
Vincent Jacquemin <vincent@impulsenow.be>
2023-03-11 21:15:07 +0100

    added dark mode,added transition to dark/light mode, added switch button, corrected menu alignment, added custom scrollbars

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,28 +1,40 @@
-
 import React from 'react';
 import LamdaButton from '../components/LamdaButton.jsx';
 import Refresh from '../components/Refresh.jsx';
+import { Scrollbar } from 'react-scrollbars-custom';
 
 const Panel = (props) => {
+  const { panelFullScreen, msNames, msMetrics, user, msLogs, setMsLogs } =
+    props;
 
-  const { panelFullScreen, msNames, msMetrics, user, msLogs, setMsLogs } = props;
- 
-  const names = []
+  const names = [];
   let i = 0;
-  if (msNames){
+  if (msNames) {
     msNames.forEach((name) => {
       i++;
-      names.push(<LamdaButton name={name} key={`${name}MsEl${i}`} msMetrics={msMetrics} user={user} className='panel-names'msLogs={msLogs} setMsLogs={setMsLogs}/>)
-    })
+      names.push(
+        <LamdaButton
+          name={name}
+          key={`${name}MsEl${i}`}
+          msMetrics={msMetrics}
+          user={user}
+          className='panel-names'
+          msLogs={msLogs}
+          setMsLogs={setMsLogs}
+        />
+      );
+    });
   }
 
-
-  
-    return(
-      <div id="panel-wrapper" className={panelFullScreen? 'full-screen' : 'collapse-screen'}>
-        <Refresh /><br />
-        {names}
-      </div>
-    )
-  }
-  export default Panel;
+  return (
+    <div
+      id='panel-wrapper'
+      className={panelFullScreen ? 'full-screen' : 'collapse-screen'}
+    >
+      <Refresh />
+      <br />
+      <Scrollbar style={{ width: '100%', height: '100%' }}>{names}</Scrollbar>
+    </div>
+  );
+};
+export default Panel;
