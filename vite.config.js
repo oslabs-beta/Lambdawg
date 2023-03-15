87920436db6e44bddde4c58a95c3efaf35cf4ec5
EPSparky <ericayeong94@gmail.com>
2023-03-15 11:30:35 -0400

    edited vite config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    hmr: {
-      host: "0.0.0.0",
-      port: 3000,
-    },
+    // hmr: {
+    //   host: "0.0.0.0",
+    //   port: 3000,
+    // },
     proxy: {
       "/api": {
         target: "http://localhost:3000",
