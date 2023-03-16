2d56c343fec6644fb81e1b5252d7d1c6d97a5d93
Chanda Gonet <chandagonet@gmail.com>
2023-03-16 13:16:51 -0400

    Add scroll to chart container

--- server/controllers/tracesController.js
+++ server/controllers/tracesController.js
@@ -17,7 +17,7 @@ tracesController.getTraces = async (req, res, next) => {
   console.log('in tracescontroller getTraces');
 
   redisClient
-    .get('LambdaTracesss' + req.body.arn)
+    .get('LambdaTraces' + req.body.arn)
     .then((data) => JSON.parse(data))
     .then((data) => {
       console.log(data);
